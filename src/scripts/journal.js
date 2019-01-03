f1028fc957a19f1a236b24a23aef77a0ba174d7e
Ashwin Prakash <ashp94@gmail.com>
2019-01-03 13:00:01 -0600

    2nd commit

---
+++ src/scripts/journal.js
@@ -0,0 +1,32 @@
+const journalEntries = [
+    {
+        date: "12/01/2018",
+        concept: "Array methods",
+        entry: "We learned about 4 different array methods today. forEach made sense, but the others still confuse me.",
+        mood: "Ok"
+    },
+    {
+        date: "12/14/2018",
+        concept: "Browserify",
+        entry: "We learned about what browserify does and why it's important",
+        mood: "good"
+    },
+    {
+        date: "01/03/2019",
+        concept: "Cohort 28 Demo Day",
+        entry: "We learned about what cohort 28 did for their frontend capstone",
+        mood: "hopeful"
+    }
+]
+
+const eBuilder = {
+    appendEntryform(){
+        let entryForm = `
+
+        `
+    }
+}
+
+const jEntries = document.querySelector("#jContainer")
+
+
